0cfe47b2c530ddbbbe5d6c14f0002dc6f11a5ffd
souhail cherif <cherifsouhail@outlook.fr>
2023-08-17 15:14:23 +0100

    m

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+         sh 'mvn clean package sonar:sonar -Dsonar.login=squ_3d6f3866ff24fa4187b8dc4b4adee2534bb7eef5'
     }
 }
