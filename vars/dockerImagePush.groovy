88444168817c8146003c0dc70320ddf28771bfe7
souhail cherif <cherifsouhail@outlook.fr>
2023-08-31 11:34:56 +0100

    m

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,4 +1,4 @@
-/*def call(String project, String ImageTag, String hubUser){
+def call(String project, String ImageTag, String hubUser){
     withCredentials([usernamePassword(
             credentialsId: "docker",
             usernameVariable: "USER",
@@ -8,13 +8,13 @@
     }
     sh "docker image push ${hubUser}/${project}:${ImageTag}"
     sh "docker image push ${hubUser}/${project}:latest"   
-}*/
+}
 
 
-def call(String aws_account_id, String region, String ecr_repoName){
+/*def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
-}
+}*/
