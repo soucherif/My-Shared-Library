73aaec4740d6fa856cbc5cccf31dc76e76f19682
souhail cherif <cherifsouhail@outlook.fr>
2023-09-06 22:58:44 +0100

    m

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,7 +10,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     sudo chmod 777 /var/run/docker.sock
+     sudo -S chmod 777 /var/run/docker.sock
      docker build -t ${ecr_repoName} .
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
