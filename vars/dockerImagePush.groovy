011ce15d396e67c47d547d02dba8518f1523208c
souhail cherif <cherifsouhail@outlook.fr>
2023-09-09 11:46:19 +0100

    m

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -11,14 +11,14 @@
 }*/
 
 
-def call(String aws_account_id, String region, String ecr_repoName){
+/*def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     $ECR_PASSWORD=$(aws ecr get-login-password --region us-east-1)
+     ECR_PASSWORD=$(aws ecr get-login-password --region us-east-1)
      docker login --username AWS --password "$ECR_PASSWORD" 454709238396.dkr.ecr.us-east-1.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
-}
+}*/
 
 
 //ECR_PASSWORD=$(aws ecr get-login-password --region us-east-1)
@@ -27,3 +27,23 @@ def call(String aws_account_id, String region, String ecr_repoName){
 //docker login --username AWS --password "$ECR_PASSWORD" 454709238396.dkr.ecr.us-east-1.amazonaws.com
 
 //aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+
+
+
+
+def call(String aws_account_id, String region, String ecr_repoName) {
+    
+    def ecrPasswordCommand = "aws ecr get-login-password --region us-east-1".execute()
+    ecrPasswordCommand.waitFor()
+
+    if (ecrPasswordCommand.exitValue() == 0) {
+        def ECR_PASSWORD = ecrPasswordCommand.text.trim()
+        
+        sh """
+            docker login --username AWS --password '$ECR_PASSWORD' 454709238396.dkr.ecr.us-east-1.amazonaws.com
+            docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+        """
+    } else {
+        error "Failed to get ECR login password"
+    }
+}
