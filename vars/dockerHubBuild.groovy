73bf48f4ef6b9c837802847baff996d105781b96
souhail cherif <cherifsouhail@outlook.fr>
2023-09-25 13:24:38 +0100

    m

--- vars/dockerHubBuild.groovy
+++ vars/dockerHubBuild.groovy
@@ -1,6 +1,7 @@
 def call(String imageName, String imageTag, String hubUser){
     
      sh """
+      sudo chmod 777 /var/run/docker.sock 
       docker image build -t ${hubUser}/${imageName} . 
       docker image tag ${hubUser}/${imageName} ${hubUser}/${imageName}:${imageTag}
       docker image tag ${hubUser}/${imageName} ${hubUser}/${imageName}:latest
