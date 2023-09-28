bf576e048c9aa94fc0be91dbd4e35198d75c39d6
souhail cherif <cherifsouhail@outlook.fr>
2023-09-28 21:50:32 +0100

    m

--- vars/dockerHbuImageCleanUp.groovy
+++ vars/dockerHbuImageCleanUp.groovy
@@ -1,7 +1,9 @@
 def call(String imageName, String imageTag, String hubUser){
     
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-       sh "docker login -u '$USER' -p '$PASS'"
+       
+       sh "echo \$PASS | docker login -u \$USER --password-stdin"
+       //sh "docker login -u '$USER' -p '$PASS'"
     }
      sh """
       docker rmi ${hubUser}/${imageName}:${imageTag}
