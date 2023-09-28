5949005e6b0332b896cf5b2b590b8a1d1e77846a
souhail cherif <cherifsouhail@outlook.fr>
2023-09-28 22:22:34 +0100

    m

--- vars/dockerHubImagePush.groovy
+++ vars/dockerHubImagePush.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String imageTag, String hubUser){
     
     
-    withCredentials([usernamePassword(credentialsId: "dockerhub", passwordVariable: "PASS", usernameVariable: "USER")]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
     
         sh " docker login -u ${USER} -p ${PASS}"
        //sh "docker login -u '$USER' -p '$PASS' "
