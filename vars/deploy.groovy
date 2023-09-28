bd34d11e88d7ac0705bdfc7d29e332514ae797d1
souhail cherif <cherifsouhail@outlook.fr>
2023-09-28 11:20:21 +0100

    m

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,5 +1,6 @@
 def call() {
-    withKubeConfig(caCertificate: '', clusterName: 'minikube', contextName: 'minikube', credentialsId: '', namespace: 'argocd', restrictKubeConfigAccess: false, serverUrl: 'https://kubernetes.default.svc') {
+    withKubeConfig(caCertificate: '', clusterName: 'minikube', contextName: 'minikube', credentialsId: '', namespace: 'argocd', restrictKubeConfigAccess: false, serverUrl: 'https://192.168.49.2:8443
+') {
     sh " kubectl apply -f atm-app.yaml" 
 }
       
