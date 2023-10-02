71a6cb83d55d80dd515fcd38361c3832046207c7
souhail cherif <cherifsouhail@outlook.fr>
2023-10-02 22:52:17 +0100

    last commit'

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,13 @@
 
 
-def call( String image_Name, String image_Tag){
+def call( String aws_account_id, String region, String ecr_repoName, String image_Tag){
     
     
     sh """
      
-     docker build -t ${image_Name}:${image_Tag} .
-     
+     docker build -t ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName} .
+     docker image tag ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${image_Tag}
+     docker image tag ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
 //sudo chmod 777 /var/run/docker.sock
