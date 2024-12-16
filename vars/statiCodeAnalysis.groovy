5381586bb0c9ebcb9f70abe8a19a69bdaad2be8d
CHERIF Souhail <101606291+soucherif@users.noreply.github.com>
2024-12-16 16:23:17 +0100

    Update statiCodeAnalysis.groovy
    change token

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar -Dsonar.login=squ_50d10cde9a0e5ede4bb31557c5d1bb551b70c556'
+         sh 'mvn clean package sonar:sonar -Dsonar.login=squ_ac15a2d9b2a3072e4a65bd46aeb71f54c85be58d'
     }
 }
